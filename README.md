cedbb668eb551cd54c42819b9c16c6fb04396673
codeGame1 <70308307+codeGame1@users.noreply.github.com>
2020-08-27 12:38:01 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+<body style="background:linear-gradient(blue, green)">  
+  <h1>Welcome to GAMEfire,</h1>
+  <p>In GAMEfire you can play games for FREE</p>
+  <p>To goto <a href="https://codegame2.github.io/GAMEfire/">GAMEfire</a> NOW!</p>
+</body>
---
+++ script.js
@@ -0,0 +1,64 @@
+var Likes1 = 0;
+var Dislikes1 = 0;
+
+function like1() {
+Likes1 += 1;
+document.getElementById("like1").innerHTML = Likes1;
+}
+
+function dislike1() {
+Dislikes1 += 1;
+document.getElementById("dislike1").innerHTML = Dislikes1;
+}
+
+var Likes2 = 0;
+var Dislikes2 = 0;
+
+function like2() {
+Likes2 += 1;
+document.getElementById("like2").innerHTML = Likes2;
+}
+
+function dislike2() {
+Dislikes2 += 1;
+document.getElementById("dislike2").innerHTML = Dislikes2;
+}
+
+var Likes3 = 0;
+var Dislikes3 = 0;
+
+function like3() {
+Likes3 += 1;
+document.getElementById("like3").innerHTML = Likes3;
+}
+
+function dislike3() {
+Dislikes3 += 1;
+document.getElementById("dislike3").innerHTML = Dislikes3;
+}
+
+var Likes4 = 0;
+var Dislikes4 = 0;
+
+function like4() {
+Likes4 += 1;
+document.getElementById("like4").innerHTML = Likes4;
+}
+
+function dislike4() {
+Dislikes4 += 1;
+document.getElementById("dislike4").innerHTML = Dislikes4;
+}
+
+var Likes5 = 0;
+var Dislikes5 = 0;
+
+function like5() {
+Likes5 += 1;
+document.getElementById("like5").innerHTML = Likes5;
+}
+
+function dislike5() {
+Dislikes5 += 1;
+document.getElementById("dislike5").innerHTML = Dislikes5;
+}
